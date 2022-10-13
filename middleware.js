6f870de8d58df777493dd491a804519b347d643c
Jaden Baptista <jaden@baptista.dev>
2022-10-13 15:11:50 -0400

    use coords from middleware

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ export const middleware = async request => {
 	const response = NextResponse.next();
 	response.cookies.set(
 		'currentCoords',
-		`${request.geo.latitude ?? "32.77807830133121"}, ${request.geo.longitude ?? "-79.92519141494746"}`
+		`${request.geo.latitude /* ?? "32.77807830133121" */}, ${request.geo.longitude /* ?? "-79.92519141494746" */}`
 	);
 	return response;
 };
