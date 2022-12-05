4d6cdf40a5d526db1099e370cfd3d3a082044ead
Jaden Baptista <jaden@baptista.dev>
2022-12-05 13:29:52 -0500

    fix middleware to new netlify next version

--- middleware.js
+++ middleware.js
@@ -6,11 +6,7 @@ export const config = {
 
 export const middleware = async (request) => {
 	const middlewareRequest = new MiddlewareRequest(request);
-
 	const response = await middlewareRequest.next();
-
-	console.log(request.geo); // prints { country: "US", region: "SC", city: "Summerville" }
-	console.log(middlewareRequest.geo); // prints { country: "US", region: "SC", city: "Summerville" }
 
 	response.cookies.set(
 		'currentCoords',
